f73ee1e7b3d3ef22c8bee2e609144e6a7dea9b0c
André Maree <ammaree@users.noreply.github.com>
2020-12-03 11:36:35 +0200

    Update copyright dates

--- m90e26.c
+++ m90e26.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 AM Maree/KSS Technologies (Pty) Ltd.
+ * Copyright 2018-20 AM Maree/KSS Technologies (Pty) Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without restriction,
